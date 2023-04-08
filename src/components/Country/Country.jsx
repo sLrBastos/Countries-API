c4163a7c4c33f631de31aa944317d74d2eee720e
sLrBastos <cerejobastosa7x@gmail.com>
2023-04-08 16:29:06 +0100

    fix border

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -14,9 +14,9 @@ const Country = ({country}) => {
           <li>Name: {name}</li>
           <li>Capital: {capital}</li>
           <li>Region: {region}</li>
-          <li>Population: {population}</li>
+          {/* <li>Population: {population}</li>
           <li>Area(Km): {area} </li>
-          <li>Borders: {borders + ("")}</li>
+          <li>Borders: {borders + ("")}</li> */}
           {/* <li>Currencies: {currencies.map(currency => <div>{currency.name} ({currency.symbol})</div>)}</li>
           <li>Languages: {languages.map( language => <div>{language.name} </div>)}</li> */}
         </ul>
--- src/pages/CountryDetails/CountryDetails.jsx
+++ src/pages/CountryDetails/CountryDetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import {useParams} from "react-router-dom"
 import { useState } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const CountryDetails = () => {
     const {name} = useParams() 
@@ -11,8 +12,8 @@ const CountryDetails = () => {
         axios
             .get(`https://restcountries.com/v2/name/${name}`)
             .then((response) => {
-                setCountry(response.data)
-                // console.log(response.data)
+                setCountry(response.data[0])
+                // console.log(response.data[0])
             })
     }
 
@@ -24,6 +25,7 @@ const CountryDetails = () => {
     <div>CountryDetails
         { country ? (
             <div>
+                {/* {console.log(country.borders.map((border, index) => <div> {border}</div>))} */}
                 <img src={country.flag} alt={country.name} />
                 <ul>
                 <li>Name: {country.name}</li>
@@ -31,9 +33,9 @@ const CountryDetails = () => {
                 <li>Region: {country.region}</li>
                 <li>Population: {country.population}</li>
                 <li>Area(Km): {country.area} </li>
-                <li>Borders: {country.borders + " "}</li>
+                <li>Borders:  {country.borders.map((border) => <Link to={`/countries/${country.name}`}><div> {border}</div></Link>)}</li>
                 <li>Currencies: {country.currencies.map(currency => <div>{currency.name} ({currency.symbol})</div>)}</li>
-                <li>Languages: { country.languages.map(language => <div>{language.name} </div>)}</li>
+                <li>Languages: {country.languages.map(language => <div>{language.name} </div>)}</li>
                 </ul>
             </div>
        ) :null}
